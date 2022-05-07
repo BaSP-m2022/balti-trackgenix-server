5c10cabb8e5e7ae49d3059a4000de83632cdaa89
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-05-07 09:58:32 -0300

    TG-04-access to arrays

--- src/index.js
+++ src/index.js
@@ -2,25 +2,34 @@
 import express from 'express';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const projects = require('./data/projects.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+console.log(projects[1].id);
 app.set('json spaces', 2);
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
-});
+app.get('/nicokun', (req, res) => res.json(projects));
 
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-});
+// app.get('/', async (req, res) => {
+//   res.send('hola');
+// });
+
+// app.get('/projects/:id', (req, res) => {
+//   // res.send('hola');
+//   res.status(200).json({
+//     // if(projects.id === id){
+//     //   res.send(projets);
+//     // }
+//   });
+// });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/projects', async (req, res) => {
+  res.send(projects);
 });
