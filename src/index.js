2e042fbae819dd34d61a0b3c4fbe9610eb867282
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-05-07 13:22:06 -0300

    TG-04: finished ?

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 // use "import" to import libraries
 import express from 'express';
-
 // use "require" to import JSON files
+const uuid = require('uuid');
 const projects = require('./data/projects.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
-console.log(projects[1].id);
+// console.log(projects.employees);
 app.set('json spaces', 2);
 app.use(express.json());
 
-app.get('/nicokun', (req, res) => res.json(projects));
+// gets all projects
+app.get('/projects', (req, res) => res.json(projects));
 
-// app.get('/', async (req, res) => {
-//   res.send('hola');
-// });
-
-// app.get('/projects/:id', (req, res) => {
-//   // res.send('hola');
-//   res.status(200).json({
-//     // if(projects.id === id){
-//     //   res.send(projets);
-//     // }
-//   });
-// });
+// filter by id
+app.get('/projects/:id', (req, res) => {
+  const found = projects.some((project) => project.id === +req.params.id);
+  if (found) {
+    res.json(projects.filter((element) => (element.id === +req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No projects with id: ${req.params.id}` });
+  }
+});
+// create project
+app.post('/projects', (req, res) => {
+  const newProject = {
+    id: uuid.v4(),
+    name: req.body.name,
+    description: req.body.description,
+    status: true,
+    owner: req.body.owner,
+    pm: req.body.pm,
+    client: req.body.client,
+    dateStart: req.body.dateStart,
+    employees: req.body.employees,
+  };
+  if (!newProject.id || !newProject.name || !newProject.owner || !newProject.pm
+    || !newProject.client || !newProject.dateStart) {
+    return res.status(400).json({ msg: 'Please include id, name, owner, pm, client and date start' });
+  }
+  projects.push(newProject);
+  return res.json(projects);
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
