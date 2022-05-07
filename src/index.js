094f40e6c5a710bbbff7af668e8e4e9a9487fcf8
ilcosme <fer.c@live.com.ar>
2022-05-07 20:20:46 -0300

    TG: get employeest list, filter by param (not working)

--- src/index.js
+++ src/index.js
@@ -28,4 +28,29 @@ app.listen(port, () => {
 
 /* example to get data from postman */
 
-app.get('/api/employees', (req, res) => res.json(employees));
+app.get('/employees', (req, res) => res.json(employees));
+
+/* example to get a single employee by first name */
+app.get('/employees/:first_name', (req, res) => {
+  res.json(employees.filter((employee) => employee.first_name === req.params.first_name));
+});
+
+/* example to get a single employee by last name */
+app.get('/employees/:last_name', (req, res) => {
+  res.json(employees.filter((employee) => employee.last_name === req.params.last_name));
+});
+
+/* example to get a single employee by email */
+app.get('/employees/:email', (req, res) => {
+  res.json(employees.filter((employee) => employee.email === req.params.email));
+});
+
+/* example to get a single employee by dni */
+app.get('/employees/:dni', (req, res) => {
+  const found = employees.some((employee) => employee.dni === parseInt(req.params.dni, 10));
+  if (found) {
+    res.json(employees.filter((element) => (element.dni === parseInt(req.params.dni, 10))));
+  } else {
+    res.status(400).json({ msg: `No employees with dni: ${req.params.dni}` });
+  }
+});
