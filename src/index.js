b9c0c014c7c19b8feef0688dcaf9d73ae557b76c
ilcosme <fer.c@live.com.ar>
2022-05-10 12:33:58 -0300

    TG: Get employees and filter by dni done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // use "import" to import libraries
 import express from 'express';
+// eslint-disable-next-line import/named
+import { filterByDni, getEmployees } from './resources/employees';
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
-const employees = require('./data/employees.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -28,29 +29,6 @@ app.listen(port, () => {
 
 /* example to get data from postman */
 
-app.get('/employees', (req, res) => res.json(employees));
+app.get('/employees', getEmployees);
 
-/* example to get a single employee by first name */
-app.get('/employees/:first_name', (req, res) => {
-  res.json(employees.filter((employee) => employee.first_name === req.params.first_name));
-});
-
-/* example to get a single employee by last name */
-app.get('/employees/:last_name', (req, res) => {
-  res.json(employees.filter((employee) => employee.last_name === req.params.last_name));
-});
-
-/* example to get a single employee by email */
-app.get('/employees/:email', (req, res) => {
-  res.json(employees.filter((employee) => employee.email === req.params.email));
-});
-
-/* example to get a single employee by dni */
-app.get('/employees/:dni', (req, res) => {
-  const found = employees.some((employee) => employee.dni === parseInt(req.params.dni, 10));
-  if (found) {
-    res.json(employees.filter((element) => (element.dni === parseInt(req.params.dni, 10))));
-  } else {
-    res.status(400).json({ msg: `No employees with dni: ${req.params.dni}` });
-  }
-});
+app.get('/employees/:dni', filterByDni);
