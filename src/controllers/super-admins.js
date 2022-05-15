e4eb4b1d7f0d5852d1681e9fd2d8db1d616b5a0d
CamilaFigue <camilafigue2@gmail.com>
2022-05-14 22:38:06 -0300

    TG-15: Super-admin created controllers

--- src/controllers/super-admins.js
+++ src/controllers/super-admins.js
@@ -1,94 +1,126 @@
-const fs = require('fs');
-const superAd = require('../data/super-admins.json');
+import ModelSuperAdmin from '../models/Super-admins';
 
-export const getAllSuperAdmin = (req, res) => {
-  res.status(200).json(superAd);
-};
-
-export const addSuperAdmin = (req, res) => {
-  const newSA = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    id: req.body.id,
-  };
-  if (!newSA.firstName || !newSA.lastName || !newSA.email || !newSA.id) {
+const getAllSuperAdmin = async (req, res) => {
+  try {
+    const allSuperAdmin = await ModelSuperAdmin.find({});
+    return res.status(200).json({
+      success: true,
+      data: allSuperAdmin,
+    });
+  } catch (err) {
     return res.status(400).json({
       success: false,
-      msg: 'Please put a name, surname, email and id.',
+      msg: 'There was an error',
     });
   }
-  superAd.push(newSA);
-  fs.writeFileSync('src/data/super-admins.json', JSON.stringify(superAd));
-  return res.status(200).json({
-    success: true,
-    data: newSA,
-  });
 };
 
-export const findSuperAdmin = (req, res) => {
-  const found = superAd.find((superAdm) => superAdm.id === +req.params.id);
-  if (found) {
-    res.status(200).json({
+const crateSuperAdmin = async (req, res) => {
+  try {
+    const superAdminCreate = new ModelSuperAdmin({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      isActive: req.isActive.isActive,
+    });
+    const result = await superAdminCreate.modelSuperAdmin.save();
+    return res.status(201).json({
       success: true,
-      data: found,
+      data: result,
     });
-  } else {
-    res.status(400).json({
+  } catch (err) {
+    return res.status(400).json({
       success: false,
-      msg: `No super admin with the id of ${req.params.id}`,
+      msg: 'There was an error',
     });
   }
 };
 
-export const delSuperAdmin = (req, res) => {
-  const found = superAd.filter((superAdm) => superAdm.id !== parseInt(req.query.id, 10));
-  if (found.length === superAd.length) {
-    res.status(400).json({
-      success: false,
-      msg: `No super admin with the id of ${req.params.id}`,
-    });
-  }
-  fs.writeFile('src/data/super-admins.json', JSON.stringify(found), (err) => {
-    if (err) {
-      res.status(400).json({
+const updateSuperAdmin = async (req, res) => {
+  try {
+    if (!req.params) {
+      return res.status(400).json({
         success: false,
-        msg: (err),
+        msg: 'Missing id parameter',
       });
     }
-  });
-  res.status(200).json({
-    success: true,
-    data: found,
-  });
-};
-
-export const editSuperAdmin = (req, res) => {
-  const saId = parseInt(req.query.id, 10);
-  const saData = req.body;
-  const found = superAd.some((sa) => sa.id === saId);
-  if (found) {
-    superAd.forEach((sa, index) => {
-      if (sa.id === saId) {
-        superAd[index].firstName = saData.firstName;
-        superAd[index].lastName = saData.lastName;
-        superAd[index].email = saData.email;
-        fs.writeFile('src/data/super-admins.json', JSON.stringify(superAd), (err) => {
-          if (err) {
-            res.send(err);
-          }
-        });
-      }
+    const result = await ModelSuperAdmin.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        msg: 'The Super Admin has not been found',
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: result,
     });
-  } else {
-    res.status(400).json({
+  } catch (err) {
+    return res.status(400).json({
       success: false,
-      msg: `SA id ${saId} not found`,
+      msg: 'There was an error',
     });
   }
-  res.status(200).json({
-    success: true,
-    msg: (`sa id ${saData.id} edited`),
-    data: found,
-  });
+};
+
+const findSuperAdminById = async (req, res) => {
+  try {
+    if (!req.params) {
+      return res.status(404).json({
+        success: false,
+        msg: 'You must specify an id',
+      });
+    }
+    const { id } = req.params;
+    const result = await ModelSuperAdmin.findById(id);
+    return res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      msg: 'There was an error',
+    });
+  }
+};
+
+const deleteSuperAdmin = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      return res.status(400).json({
+        success: false,
+        msg: 'Missing id parameter',
+      });
+    }
+    const result = await ModelSuperAdmin.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        msg: 'The Super Admin has not been found',
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      msg: 'There was an error',
+    });
+  }
+};
+
+export default {
+  getAllSuperAdmin,
+  crateSuperAdmin,
+  updateSuperAdmin,
+  deleteSuperAdmin,
+  findSuperAdminById,
 };
