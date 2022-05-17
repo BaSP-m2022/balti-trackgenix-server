44668176191b4e9aa2397f1ce43fa92630d5e13c
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-05-17 18:31:49 -0300

    TG-22: Edit and update controllers: updateProjectById, getProjectsByStatus, getAllProjects, getProjectById, createNewProject

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,157 +1,115 @@
-const fs = require('fs');
-const projects = require('../data/projects.json');
+import Projects from '../models/Projects';
 
-export const putById = (req, res) => {
-  const { id } = req.params;
+export const updateProjectById = async (req, res) => {
+  try {
+    const projectToUpdate = await Projects.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
 
-  const {
-    name, description, owner, pm, client, dateStart, employees,
-  } = req.body;
-
-  let validateStatus = null;
-
-  if (req.body.status === true || req.body.status === false) {
-    validateStatus = req.body.status;
-  }
-
-  const updatedProject = {
-    id: parseInt(id, 10),
-    name: name || '',
-    description: description || '',
-    status: validateStatus,
-    owner: owner || '',
-    pm: pm || '',
-    client: client || '',
-    dateStart: dateStart || '',
-    employees: employees || '',
-  };
-
-  const projectIndex = projects.findIndex((proj) => proj.id === parseInt(id, 10));
-
-  if (projectIndex !== -1) {
-    projects[projectIndex] = updatedProject;
-    fs.writeFileSync('./src/data/projects.json', JSON.stringify(projects));
+    if (!projectToUpdate) {
+      return res.status(400).json({
+        msg: `Project not found for id: ${req.params.id}`,
+        error: true,
+      });
+    }
     return res.status(200).json({
-      success: true,
-      data: updatedProject,
+      msg: `Project with id ${req.params.id} has been successfully updated!`,
+      data: projectToUpdate,
+      error: false,
     });
-  }
-  return res.status(400).json({
-    success: false,
-    msg: ('Project not found'),
-  });
-};
-
-export const deleteById = (req, res) => {
-  const num = parseInt(req.params.id, 10);
-
-  const ids = projects.map((project) => project.id);
-
-  const index = ids.indexOf(num);
-
-  if (index !== -1) {
-    projects.splice(index, 1);
-    fs.writeFileSync('src/data/projects.json', JSON.stringify(projects));
-    return res.status(200).json({
-      success: true,
-      data: projects,
-    });
-  }
-  return res.status(400).json({
-    success: false,
-    msg: ('Project not found'),
-  });
-};
-
-export const putEmployee = (req, res) => {
-  const { id } = req.params;
-
-  const {
-    name, roll, salary, hoursInProjects, rates,
-  } = req.body;
-
-  const newEmployee = {
-    name: name || '',
-    idEmployee: req.body.id || '',
-    roll: roll || '',
-    salary: salary || '',
-    hoursInProjects: hoursInProjects || '',
-    rates: rates || '',
-  };
-
-  const project = projects.find((proj) => proj.id === parseInt(id, 10));
-  const projectIndex = projects.findIndex((proj) => proj.id === parseInt(id, 10));
-
-  if (project) {
-    project.employees.push(newEmployee);
-    projects[projectIndex] = project;
-    fs.writeFileSync('./src/data/projects.json', JSON.stringify(projects));
-    return res.status(200).json({
-      success: true,
-      data: project,
-    });
-  }
-  return res.status(400).json({
-    success: false,
-    msg: ('Project not found'),
-  });
-};
-
-export const getByStatus = (req, res) => {
-  const { status } = req.params;
-  const projectsFilter = projects.filter((proj) => proj.status.toString() === status);
-  if (projectsFilter) {
-    return res.status(200).json({
-      success: true,
-      data: projectsFilter,
-    });
-  }
-  return res.status(400).json({
-    success: false,
-    msg: ('Project not found'),
-  });
-};
-
-export const allProjects = (req, res) => res.json(projects);
-
-export const filterById = (req, res) => {
-  const found = projects.find((project) => project.id === +req.params.id);
-  if (found) {
-    res.status(200).json({
-      success: true,
-      data: found,
-    });
-  } else {
-    res.status(400).json({
-      success: false,
-      msg: 'Id not found',
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'There was an error',
+      data: error,
+      error: true,
     });
   }
 };
 
-export const createProject = (req, res) => {
-  const newProject = {
-    id: Math.round(Math.random() * 10000),
-    name: req.body.name,
-    description: req.body.description,
-    status: true,
-    owner: req.body.owner,
-    pm: req.body.pm,
-    client: req.body.client,
-    dateStart: req.body.dateStart,
-    employees: req.body.employees,
-  };
-  if (!newProject.id || !newProject.name || !newProject.owner || !newProject.pm
-    || !newProject.client || !newProject.dateStart) {
+export const getProjectsByStatus = async (req, res) => {
+  try {
+    const activeProjects = await Projects.find({ isActive: req.params.status });
+    if (activeProjects.length) {
+      return res.status(200).json({
+        msg: 'Obtained projects!',
+        data: activeProjects,
+        error: false,
+      });
+    }
     return res.status(400).json({
-      success: false,
-      msg: 'Please include id, name, owner, pm, client and date start.',
+      msg: `Projects not found for status: ${req.params.status}`,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'There was an error',
+      data: error,
+      error: true,
     });
   }
-  projects.push(newProject);
-  fs.writeFileSync('./src/data/projects.json', JSON.stringify(projects));
-  return res.status(200).json({
-    success: true,
-    data: newProject,
-  });
+};
+
+export const getAllProjects = async (req, res) => {
+  try {
+    const allProjects = await Projects.find({});
+
+    return res.status(200).json({
+      msg: 'Successful query',
+      data: allProjects,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'There was an error',
+      data: error,
+      error: true,
+    });
+  }
+};
+
+export const getProjectById = async (req, res) => {
+  try {
+    const wantedProject = await Projects.findById(req.params.id);
+
+    if (wantedProject) {
+      return res.status(200).json({
+        msg: 'Successful search!',
+        data: wantedProject,
+        error: false,
+      });
+    }
+    return res.status(400).json({
+      msg: `Project not found for id: ${req.params.id}`,
+      error: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'There was an error',
+      data: error,
+      error: true,
+    });
+  }
+};
+
+export const createNewProject = async (req, res) => {
+  try {
+    const newProjects = new Projects({
+      ...req.body,
+    });
+    await newProjects.save();
+
+    return res.status(201).json({
+      msg: 'New Project successfully created',
+      data: newProjects,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      msg: 'There was an error',
+      data: error,
+      error: false,
+    });
+  }
 };
