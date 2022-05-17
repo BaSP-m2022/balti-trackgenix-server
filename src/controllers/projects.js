19386f395034658939cc6f367de4860f67254a22
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-05-17 20:45:25 -0300

    TG-22: edit controllers/projects.js to leave only the features I worked on: updateProjectById, getProjectsByStatus, getProjectById

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -51,24 +51,6 @@ export const getProjectsByStatus = async (req, res) => {
   }
 };
 
-export const getAllProjects = async (req, res) => {
-  try {
-    const allProjects = await Projects.find({});
-
-    return res.status(200).json({
-      msg: 'Successful query',
-      data: allProjects,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      msg: 'There was an error',
-      data: error,
-      error: true,
-    });
-  }
-};
-
 export const getProjectById = async (req, res) => {
   try {
     const wantedProject = await Projects.findById(req.params.id);
@@ -89,27 +71,6 @@ export const getProjectById = async (req, res) => {
       msg: 'There was an error',
       data: error,
       error: true,
-    });
-  }
-};
-
-export const createNewProject = async (req, res) => {
-  try {
-    const newProjects = new Projects({
-      ...req.body,
-    });
-    await newProjects.save();
-
-    return res.status(201).json({
-      msg: 'New Project successfully created',
-      data: newProjects,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      msg: 'There was an error',
-      data: error,
-      error: false,
     });
   }
 };
