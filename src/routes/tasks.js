9896b9762990fa3f88148df2e8a7b993dbdab814
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-05-16 21:50:09 -0300

    TG-24: postman-collection and validations of put and post with joi

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,13 +2,14 @@ import express from 'express';
 import {
   getTasks, findTask, addTask, editTask, deleteTask,
 } from '../controllers/tasks';
+import { validatePost, validatePut } from '../validations/tasks';
 // adminds controllers
 const router = express.Router();
 
 router.get('/', getTasks);
 router.get('/:id', findTask);
-router.post('/', addTask);
-router.put('/:id', editTask);
+router.post('/', validatePost, addTask);
+router.put('/:id', validatePut, editTask);
 router.delete('/:id', deleteTask);
 
 export default router;
--- src/validations/tasks.js
+++ src/validations/tasks.js
@@ -1,20 +1,39 @@
-const joi = require('joi');
+import joi from 'joi';
 
-const authSchemaPostAndPut = joi.object({
-  id: joi.string().required(),
-  employee_id: joi.string(),
-  pm_id: joi.string().required(),
-  tittle: joi.string().required(),
-  description: joi.string().max(50),
-  date: joi.date(),
-  done: joi.boolean().required(),
-});
+export const validatePost = (req, res, next) => {
+  const schemaValidate = joi.object({
+    employee_id: joi.string(),
+    pm_id: joi.string().required(),
+    tittle: joi.string().required(),
+    description: joi.string().max(50),
+    date: joi.date(),
+    done: joi.boolean().required(),
+  });
+  const result = schemaValidate.validate(req.body);
+  if (result.error) {
+    return res.status(400).json({
+      message: 'Error, incorrectly entered the data of the body.',
+      error: true,
+    });
+  }
+  return next();
+};
 
-const authSchemaId = joi.object({
-  id: joi.string().required(),
-});
-
-exports = {
-  authSchemaPostAndPut,
-  authSchemaId,
+export const validatePut = (req, res, next) => {
+  const schemaValidate = joi.object({
+    employee_id: joi.string(),
+    pm_id: joi.string(),
+    tittle: joi.string(),
+    description: joi.string().max(50),
+    date: joi.date(),
+    done: joi.boolean(),
+  });
+  const result = schemaValidate.validate(req.body);
+  if (result.error) {
+    return res.status(400).json({
+      message: 'Error, incorrectly entered the data of the body.',
+      error: true,
+    });
+  }
+  return next();
 };
