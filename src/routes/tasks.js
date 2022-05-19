a29221603caa13b53b568ce2da3e271bff550619
Agustín Ferrarello <ferrarello.agustin@gmail.com>
2022-05-19 15:42:57 -0300

    ....

---
+++ src/routes/tasks.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {
+  getTasks, findTask, addTask, editTask, deleteTask,
+} from '../controllers/tasks';
+import { validatePost, validatePut } from '../validations/tasks';
+
+const router = express.Router();
+
+router.get('/', getTasks);
+router.get('/:id', findTask);
+router.post('/', validatePost, addTask);
+router.put('/:id', validatePut, editTask);
+router.delete('/:id', deleteTask);
+
+export default router;
