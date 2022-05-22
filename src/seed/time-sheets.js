b92c9c045eb7803989e7acc852018ae2aea2715b
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-05-22 03:04:30 -0300

    TG-37: added get all timesheets tests.

--- src/seed/time-sheets.js
+++ src/seed/time-sheets.js
@@ -1,12 +1,76 @@
 export default [{
-  _id: '6285b864f52d378096258169',
-  employee: '6287c08beee9276577d53b1f',
-  project: '62893107ea8586fcd9e96120',
+  employee: {
+    $oid: '6289c467fc13ae72d60000c8',
+  },
+  project: {
+    $oid: '6289c467fc13ae72d60000c9',
+  },
   role: 'qa',
-  date: '2022-05-19T03:24:20.863Z',
-  rate: 23,
-  workedHours: 24,
-  description: 'Worked hours.',
-  __v: 0,
-  task: '6288fa66a52cdee44fee0144',
+  date: '6/21/2021',
+  rate: 10,
+  workedHours: 33,
+  description: 'Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+  task: {
+    $oid: '6289c467fc13ae72d60000ca',
+  },
+}, {
+  employee: {
+    $oid: '6289c467fc13ae72d60000cb',
+  },
+  project: {
+    $oid: '6289c467fc13ae72d60000cc',
+  },
+  role: 'tl',
+  date: '7/19/2021',
+  rate: 5,
+  workedHours: 21,
+  description: 'Norem, vitae mattis nibh ligula nec sem.',
+  task: {
+    $oid: '6289c467fc13ae72d60000cd',
+  },
+}, {
+  employee: {
+    $oid: '6289c467fc13ae72d60000ce',
+  },
+  project: {
+    $oid: '6289c467fc13ae72d60000cf',
+  },
+  role: 'tl',
+  date: '10/26/2021',
+  rate: 10,
+  workedHours: 64,
+  description: 'Consequat ut, nulla.',
+  task: {
+    $oid: '6289c467fc13ae72d60000d0',
+  },
+}, {
+  employee: {
+    $oid: '6289c467fc13ae72d60000d1',
+  },
+  project: {
+    $oid: '6289c467fc13ae72d60000d2',
+  },
+  role: 'qa',
+  date: '6/24/2021',
+  rate: 7,
+  workedHours: 45,
+  description: 'Te in, tempus sit amet, sem.',
+  task: {
+    $oid: '6289c467fc13ae72d60000d3',
+  },
+}, {
+  employee: {
+    $oid: '6289c467fc13ae72d60000d4',
+  },
+  project: {
+    $oid: '6289c467fc13ae72d60000d5',
+  },
+  role: 'tl',
+  date: '9/7/2021',
+  rate: 3,
+  workedHours: 32,
+  description: 'Nulla ultrices aliquet. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.',
+  task: {
+    $oid: '6289c467fc13ae72d60000d6',
+  },
 }];
--- src/test/time-sheets.test.js
+++ src/test/time-sheets.test.js
@@ -1,15 +1,27 @@
 import request from 'supertest';
 import app from '../index';
 import Timesheet from '../models/Time-sheets';
-import timeSheetsSeed from '../seed/time-sheets';
+import timesheetsSeed from '../seed/time-sheets';
 
 beforeAll(async () => {
-  await Timesheet.collection.insertMany(timeSheetsSeed);
+  await Timesheet.collection.insertMany(timesheetsSeed);
 });
 
 describe('/GET /timesheets', () => {
-  test('return status 200 timesheets', async () => {
+  test('Response should return a status 200', async () => {
     const response = await request(app).get('/timesheets').send();
     expect(response.error).toBeFalsy();
+  });
+  test('Response should return a status 400', async () => {
+    const response = await request(app).get('/timesheet').send();
+    expect(response.status).toBe(404);
+  });
+  test('Response should return a non empty body content', async () => {
+    const response = await request(app).get('/timesheets').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+  test('Response should return minimum body content', async () => {
+    const response = await request(app).get('/timesheets').send();
+    expect(Object.keys(response.body.data[0]).length).toBeGreaterThan(5);
   });
 });
