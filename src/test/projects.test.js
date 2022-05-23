c8dc0ea86e10338009c1f61458f3486aa8512ed4
ilcosme <fer.c@live.com.ar>
2022-05-23 20:30:34 -0300

    TG-36: added errors validations, all testing working

--- src/test/projects.test.js
+++ src/test/projects.test.js
@@ -3,23 +3,25 @@ import app from '../index';
 import Projects from '../models/Projects';
 import projectsSeed from '../seed/projects';
 
-// let projectID;
-
 beforeAll(async () => {
   await Projects.collection.insertMany(projectsSeed);
 });
 
 describe('/GET /projects', () => {
-  test('return status 200 projects', async () => {
-    const response = await request(app).get('/projects').send();
-    expect(response.error).toBeFalsy();
-  });
   test('Response should return one project', async () => {
-    const response = await request(app).get('/projects').send();
-    expect(response.body.data.length).toBeGreaterThan(0);
+    const response = await request(app).get('/projects/62891944b389642a7f13ca58').send();
+    expect(response.body.data.length).not.toBe(null);
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
   });
-/*  test('Project updated', async () => {
-    const response = await request(app).put(`/projects/${projectID}`).send({
+  test('Wrong path data, should return error', async () => {
+    const response = await request(app).get('/projects/62891944b389642a').send();
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.msg).toEqual('There was an error');
+  });
+  test('Project updated', async () => {
+    const response = await request(app).put('/projects/62891944b389642a7f13ca58').send({
       projectName: 'Fourth Project',
       description: 'Description of project',
       isActive: false,
@@ -36,12 +38,41 @@ describe('/GET /projects', () => {
         },
       ],
     });
-
-    expect(response.statusCode).toBe(200);
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+  });
+  test('Wrong id data, should return not found', async () => {
+    const response = await request(app).put('/projects/62891944b389').send({
+      projectName: 'Fourth Project',
+      description: 'Description of project',
+      isActive: false,
+      admin: '6287b9da26cff823b1f9055b',
+      client: 'Nicolas Costanza',
+      startDate: 2015 - 25 - 10,
+      endDate: 2021 - 25 - 10,
+      employees: [
+        {
+          employeeId: '6287e6f01c1709ee93503342',
+          role: 'TL',
+          rate: 5000,
+          hoursInProject: 700,
+        },
+      ],
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.msg).toEqual('Project not found for id: 62891944b389');
   });
   test('Project deleted', async () => {
-    const response = await request(app).delete(`/projects/${projectID}`);
-    expect(response.statusCode).toBe(200);
+    const response = await request(app).delete('/projects/62891944b389642a7f13ca58');
+    expect(response.status).toBe(200);
     expect(response.body.error).toBe(false);
-  }); */
+    expect(response.body.msg).toEqual('Project deleted.');
+  });
+  test('Wrong path data, should return error', async () => {
+    const response = await request(app).delete('/projects/62891944b389642a7f13');
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.msg).toEqual('There was an error');
+  });
 });
