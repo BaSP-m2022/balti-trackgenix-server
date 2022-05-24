552d0e433feb63a5511b1c8ca85e1748137f1517
Agustín Ferrarello <ferrarello.agustin@gmail.com>
2022-05-24 18:27:17 -0300

    TG-34: id's changed and some others tests.

--- src/seed/projects.js
+++ src/seed/projects.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export default [{
-  _id: mongoose.Types.ObjectId('6287f93beee9276577d60c1f'),
+  _id: mongoose.Types.ObjectId('628cf237305204bf7d672d7b'),
   projectName: 'First Project',
   description: 'Description of project',
   isActive: true,
@@ -20,7 +20,7 @@ export default [{
   __v: 0,
 },
 {
-  _id: mongoose.Types.ObjectId('6288k93beee9276577d60c1s'),
+  _id: mongoose.Types.ObjectId('628cf23dfb13310e147c4fdb'),
   projectName: 'Second Project',
   description: 'Description of project',
   isActive: true,
@@ -39,7 +39,7 @@ export default [{
   __v: 0,
 },
 {
-  _id: mongoose.Types.ObjectId('1998k93beee9276577d60c4u'),
+  _id: mongoose.Types.ObjectId('628cf2442fed32f30b57dc75'),
   projectName: 'Third Project',
   description: 'Description of project',
   isActive: false,
@@ -53,63 +53,6 @@ export default [{
       role: 'PM',
       rate: 1000,
       hoursInProject: 200,
-    },
-  ],
-  __v: 0,
-},
-{
-  _id: mongoose.Types.ObjectId('0034k93beee9276577d60c4g'),
-  projectName: 'Fourth Project',
-  description: 'Description of project',
-  isActive: false,
-  admin: '6287b9da26cff823b1f9055b',
-  client: 'Nicolas Costanza',
-  startDate: 2015 - 25 - 10,
-  endDate: 2021 - 25 - 10,
-  employees: [
-    {
-      employeeId: '6287e6f01c1709ee93503342',
-      role: 'TL',
-      rate: 5000,
-      hoursInProject: 700,
-    },
-  ],
-  __v: 0,
-},
-{
-  _id: mongoose.Types.ObjectId('9547k93beee9276577s60c2g'),
-  projectName: 'Fifth Project',
-  description: 'Description of project',
-  isActive: true,
-  admin: '6287b91426cff823b1f9055a',
-  client: 'Florencia Eusebi',
-  startDate: 2010 - 13 - 12,
-  endDate: 2020 - 13 - 12,
-  employees: [
-    {
-      employeeId: '6287c08beee9276577d53b1f',
-      role: 'PM',
-      rate: 8000,
-      hoursInProject: 1000,
-    },
-  ],
-  __v: 0,
-},
-{
-  _id: mongoose.Types.ObjectId('5432k93befe9276577s60c2r'),
-  projectName: 'Sixth Project',
-  description: 'Description of project',
-  isActive: false,
-  admin: '6287b9da26cff823b1f9055b',
-  client: 'Agustin Ferrarello',
-  startDate: 2015 - 25 - 10,
-  endDate: 2021 - 25 - 10,
-  employees: [
-    {
-      employeeId: '6287e6f01c1709ee93503342',
-      role: 'DEV',
-      rate: 7000,
-      hoursInProject: 500,
     },
   ],
   __v: 0,
--- src/test/employee.test.js
+++ src/test/employee.test.js
@@ -3,40 +3,70 @@ import request from 'supertest';
 import app from '../index';
 import Employees from '../models/Employees';
 import employeeSeed from '../seed/employees';
+import Projects from '../models/Projects';
+import projectSeed from '../seed/projects';
 
 beforeAll(async () => {
   await Employees.collection.insertMany(employeeSeed);
+  await Projects.collection.insertMany(projectSeed);
 });
+
 // get one employee tests
 describe('GET /employees', () => {
-  test('Valid employee ID should return status 200', async () => {
-    const response = await request(app).get('/employees/6288f73964ed6961bb7c2075').send();
+  test('Valid employee ID should return status 200.', async () => {
+    const response = await request(app).get('/employees/62891944b389642a7f13ca53').send();
     expect(response.status).toBe(200);
   });
-  test('ID not founded should return status 404', async () => {
+  test('Valid employee ID should return error:false.', async () => {
+    const response = await request(app).get('/employees/62891944b389642a7f13ca53').send();
+    expect(response.error).toBeFalsy();
+  });
+  test('ID not founded should return status 404.', async () => {
     const response = await request(app).get('/employees/6288f73964ed6961bb7c2032').send();
     expect(response.status).toBe(404);
   });
-  test('Invalid ID should return status 400', async () => {
+  test('ID not founded should return error:true.', async () => {
+    const response = await request(app).get('/employees/6288f73964ed6961bb7c2031').send();
+    expect(response.error).toBeTruthy();
+  });
+  test('Invalid ID should return status 400.', async () => {
     const response = await request(app).get('/employees/a22').send();
     expect(response.status).toBe(400);
   });
+  test('Invalid ID should not return error:false.', async () => {
+    const response = await request(app).get('/employees/a22').send();
+    expect(response.error).not.toBeFalsy();
+  });
 });
 
-// // edit employee tests
+// edit employee tests
 describe('PUT /employees', () => {
-  test('Edit employee with all the parameters correct should return status 200', async () => {
-    const response = await request(app).put('/employees/6287c08beee9276577d53b1f').send({
+  test('Edit employee with all the parameters correct should return status 200.', async () => {
+    const response = await request(app).put('/employees/62891944b389642a7f13ca53').send({
       firstName: 'Msssati',
       lastName: 'Weber',
       email: 'Max@gmail.com',
       password: 'Max123456',
+      // assignedProjects: mongoose.Types.ObjectId('628cf237305204bf7d672d7b'),
       isActive: true,
     });
+    // console.log(response);
     expect(response.status).toBe(200);
   });
-  test('Edit employee with an incorrect parameter should return status 400', async () => {
-    const response = await request(app).put('/employees/6287c08beee9276577d53b1f').send({
+  test('Edit employee with all the parameters correct should return error:false.', async () => {
+    const response = await request(app).put('/employees/62891944b389642a7f13ca53').send({
+      firstName: 'Msssati',
+      lastName: 'Weber',
+      email: 'Max@gmail.com',
+      password: 'Max123456',
+      // assignedProjects: mongoose.Types.ObjectId('628cf237305204bf7d672d7b'),
+      isActive: true,
+    });
+    // console.log(response);
+    expect(response.error).toBeFalsy();
+  });
+  test('Edit employee with an incorrect parameter should return status 400.', async () => {
+    const response = await request(app).put('/employees/62891944b389642a7f13ca53').send({
       firstName: 'Msssati',
       lastName: 'Fe',
       email: 'Max@gmail.com',
@@ -45,7 +75,17 @@ describe('PUT /employees', () => {
     });
     expect(response.status).toBe(400);
   });
-  test('Edit employee with an incorrect ID should return status 404', async () => {
+  test('Edit employee with an incorrect parameter should return error message.', async () => {
+    const response = await request(app).put('/employees/628cf152c7dfd0c4fe2edb9e').send({
+      firstName: 'Msssati',
+      lastName: 'Fee',
+      email: 'Max@gmail.com',
+      password: 'Max1234',
+      isActive: true,
+    });
+    expect(response.body.message).toBe('Missing data');
+  });
+  test('Edit employee with an incorrect ID should return status 404.', async () => {
     const response = await request(app).put('/employees/6287c08beee9276577d53b2f').send({
       firstName: 'Msssati',
       lastName: 'Weber',
@@ -55,20 +95,39 @@ describe('PUT /employees', () => {
     });
     expect(response.status).toBe(404);
   });
+  test('Edit employee with an incorrect ID should return error:true.', async () => {
+    const response = await request(app).put('/employees/6287c08beee9276577d53b2f').send({
+      firstName: 'Msssati',
+      lastName: 'Weber',
+      email: 'Max@gmail.com',
+      password: 'Max123456',
+      isActive: true,
+    });
+    expect(response.error).toBeTruthy();
+  });
 });
 
 // delete employee tests
 describe('DEL /employees', () => {
-  test('Delete invalid employee ID should return status 400', async () => {
+  test('Delete invalid employee ID should return status 400.', async () => {
     const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
     expect(response.status).toBe(400);
   });
-  test('Delete incorrect employee ID should return status 404', async () => {
+  test('Delete invalid employee ID should return error message.', async () => {
+    const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
+    expect(response.body.message).toEqual('There was an error');
+  });
+  test('Delete incorrect employee ID should return status 404.', async () => {
     const response = await request(app).delete('/employees/6288f73964ed6961bb7c2175').send();
     expect(response.status).toBe(404);
   });
-  test('Delete correct employee ID should return status 200', async () => {
-    const response = await request(app).delete('/employees/6288f73964ed6961bb7c2075').send();
+  test('Delete correct employee ID should return status 200.', async () => {
+    const response = await request(app).delete('/employees/62891944b389642a7f13ca53').send();
     expect(response.status).toBe(200);
+  });
+  test('Delete correct employee ID should return success message.', async () => {
+    const response = await request(app).delete('/employees/628cf152c7dfd0c4fe2edb9e').send();
+    // console.log(response);
+    expect(response.body.message).toEqual('Employee id: 628cf152c7dfd0c4fe2edb9e deleted.');
   });
 });
