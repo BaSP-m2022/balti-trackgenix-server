472f6474d4f2d5a16ac1d790f517c258c3c4024f
ilcosme <fer.c@live.com.ar>
2022-05-24 01:15:45 -0300

    TG-36: corrections to PR

--- src/test/projects.test.js
+++ src/test/projects.test.js
@@ -2,15 +2,21 @@ import request from 'supertest';
 import app from '../index';
 import Projects from '../models/Projects';
 import projectsSeed from '../seed/projects';
+import Employees from '../models/Employees';
+import employeesSeed from '../seed/employees';
+import Admins from '../models/Admins';
+import adminsSeed from '../seed/admins';
 
 beforeAll(async () => {
   await Projects.collection.insertMany(projectsSeed);
+  await Employees.collection.insertMany(employeesSeed);
+  await Admins.collection.insertMany(adminsSeed);
 });
 
 describe('/GET /projects', () => {
   test('Response should return one project', async () => {
     const response = await request(app).get('/projects/62891944b389642a7f13ca58').send();
-    expect(response.body.data.length).not.toBe(null);
+    expect(response.body.msg).toEqual('Successful search!');
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
   });
@@ -20,6 +26,9 @@ describe('/GET /projects', () => {
     expect(response.body.error).toBe(true);
     expect(response.body.msg).toEqual('There was an error');
   });
+});
+
+describe('/PUT /projects', () => {
   test('Project updated', async () => {
     const response = await request(app).put('/projects/62891944b389642a7f13ca58').send({
       projectName: 'Fourth Project',
@@ -38,6 +47,7 @@ describe('/GET /projects', () => {
         },
       ],
     });
+    expect(response.body.msg).toEqual('Project with id 62891944b389642a7f13ca58 has been successfully updated!');
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
   });
@@ -63,6 +73,9 @@ describe('/GET /projects', () => {
     expect(response.body.error).toBe(true);
     expect(response.body.msg).toEqual('Project not found for id: 62891944b389');
   });
+});
+
+describe('/DELETE /projects', () => {
   test('Project deleted', async () => {
     const response = await request(app).delete('/projects/62891944b389642a7f13ca58');
     expect(response.status).toBe(200);
