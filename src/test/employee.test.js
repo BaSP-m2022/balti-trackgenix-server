1d264b6b376fdd2590b0a3ddea4b9b555c3bdc77
Agustín Ferrarello <ferrarello.agustin@gmail.com>
2022-05-26 18:27:53 -0300

    TG-34: Last corrections.

--- src/test/employee.test.js
+++ src/test/employee.test.js
@@ -14,25 +14,16 @@ describe('GET /employees/:id', () => {
   test('Valid employee ID should return status 200.', async () => {
     const response = await request(app).get('/employees/62891944b389642a7f13ca53').send();
     expect(response.status).toBe(200);
-  });
-  test('Valid employee ID should return error:false.', async () => {
-    const response = await request(app).get('/employees/62891944b389642a7f13ca53').send();
     expect(response.error).toBeFalsy();
   });
   test('ID not founded should return status 404.', async () => {
     const response = await request(app).get('/employees/6288f73964ed6961bb7c2032').send();
     expect(response.status).toBe(404);
-  });
-  test('ID not founded should return error:true.', async () => {
-    const response = await request(app).get('/employees/6288f73964ed6961bb7c2031').send();
     expect(response.error).toBeTruthy();
   });
   test('Invalid ID should return status 400.', async () => {
     const response = await request(app).get('/employees/a22').send();
     expect(response.status).toBe(400);
-  });
-  test('Invalid ID should not return error:false.', async () => {
-    const response = await request(app).get('/employees/a22').send();
     expect(response.error).not.toBeFalsy();
   });
 });
@@ -47,17 +38,14 @@ describe('PUT /employees/:id', () => {
       isActive: true,
     });
     expect(response.status).toBe(200);
-  });
-  test('Edit employee with all the parameters correct should return error:false.', async () => {
-    const response = await request(app).put('/employees/62891944b389642a7f13ca53').send({
-      firstName: 'Msssati',
-      lastName: 'Weber',
-      email: 'Max@gmail.com',
-      password: 'Max123456',
-      isActive: true,
-    });
     expect(response.error).toBeFalsy();
+    expect(response.body.data.firstName).toEqual('Msssati');
+    expect(response.body.data.lastName).toEqual('Weber');
+    expect(response.body.data.email).toEqual('Max@gmail.com');
+    expect(response.body.data.password).toEqual('Max123456');
+    expect(response.body.data.isActive).toBe(true);
   });
+
   test('Edit employee with an incorrect parameter should return status 400.', async () => {
     const response = await request(app).put('/employees/62891944b389642a7f13ca53').send({
       firstName: 'Msssati',
@@ -67,15 +55,6 @@ describe('PUT /employees/:id', () => {
       isActive: true,
     });
     expect(response.status).toBe(400);
-  });
-  test('Edit employee with an incorrect parameter should return error message.', async () => {
-    const response = await request(app).put('/employees/628cf152c7dfd0c4fe2edb9e').send({
-      firstName: 'Msssati',
-      lastName: 'Fee',
-      email: 'Max@gmail.com',
-      password: 'Max1234',
-      isActive: true,
-    });
     expect(response.body.message).toBe('Missing data');
   });
   test('Edit employee with an incorrect ID should return status 404.', async () => {
@@ -87,46 +66,30 @@ describe('PUT /employees/:id', () => {
       isActive: true,
     });
     expect(response.status).toBe(404);
-  });
-  test('Edit employee with an incorrect ID should return error:true.', async () => {
-    const response = await request(app).put('/employees/6287c08beee9276577d53b2f').send({
-      firstName: 'Msssati',
-      lastName: 'Weber',
-      email: 'Max@gmail.com',
-      password: 'Max123456',
-      isActive: true,
-    });
     expect(response.error).toBeTruthy();
   });
 });
 
 describe('DEL /employees/:id', () => {
-  test('Delete invalid employee ID should return error message.', async () => {
-    const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
-    expect(response.status).toBe(400);
-  });
-  test('Delete invalid employee ID should return error:true.', async () => {
-    const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
-    expect(response.error).toBeTruthy();
-  });
   test('Delete invalid employee ID should return status 400.', async () => {
     const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
-    expect(response.body.message).toBe('There was an error');
+    expect(response.status).toBe(400);
+    expect(response.error).toBeTruthy();
   });
   test('Delete invalid employee ID should return error message.', async () => {
     const response = await request(app).delete('/employees/6288f73964ed6961bb7c275').send();
     expect(response.body.message).toEqual('There was an error');
   });
+
   test('Delete incorrect employee ID should return status 404.', async () => {
     const response = await request(app).delete('/employees/6288f73964ed6961bb7c2175').send();
     expect(response.status).toBe(404);
+    expect(response.error).toBeTruthy();
   });
+
   test('Delete correct employee ID should return status 200.', async () => {
     const response = await request(app).delete('/employees/62891944b389642a7f13ca53').send();
     expect(response.status).toBe(200);
-  });
-  test('Delete correct employee ID should return error:false.', async () => {
-    const response = await request(app).delete('/employees/628cf15a2ca8617119124234').send();
     expect(response.error).toBe(false);
   });
   test('Delete correct employee ID should return success message.', async () => {
