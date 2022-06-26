8659edce2c9e0d2e30f6793b83fe2f26ebf6070a
Julian Lescano <lescano.julian22@gmail.com>
2022-06-26 14:27:04 -0300

    TG-90: Auth middleware modified

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,19 +1,23 @@
 import firebase from '../helper/firebase';
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.headers;
+  // if (!token) {
+  //   return res.status(400)
+  //     .json({
+  //       message: 'Provide a token',
+  //       data: undefined,
+  //       error: true,
+  //     });
+  // }
   try {
-    const { token } = req.headers;
-    if (!token) {
-      return res.status(400)
-        .json({ message: 'Provide a token' });
-    }
-    return () => {
-      firebase.auth().verifyIdToken(token);
-      next();
-    };
+    await firebase.auth().verifyIdToken(token);
+    return next();
   } catch (error) {
     return res.status(401).json({
       message: error.toString(),
+      data: undefined,
+      error: true,
     });
   }
 };
